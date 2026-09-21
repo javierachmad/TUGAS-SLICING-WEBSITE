a66815282763952cee77ba5abf667e1f0eca3423
javierachmad <adekbudih9@gmail.com>
2026-09-21 08:19:59 +0700

    perubaha is file html js dan css

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Menampilkan isi biodata di index2.html jika elemennya ada
-    const biodata = document.getElementById("biodata");
-    if (biodata) {
-        biodata.innerHTML = `
-            <p>Halo! Saya <strong>Javier Achmad</strong>, siswa <strong>SMAN 1 Tenggarang</strong> yang senang menguji coba pembuatan website dan belajar koding secara mandiri.</p>
+  // Menampilkan isi biodata di index2.html jika elemennya ada
+  const biodata = document.getElementById("biodata");
+  if (biodata) {
+    biodata.innerHTML = `
+            <p>Halo! Saya <strong>Javier Achmad</strong>, Mahasiswa <strong> Universitas Negeri Jember</strong> yang senang menguji dan mencoba merakit sesuatu dengan menggabungkan mikrocontroller dan software</p>
         `;
-    }
+  }
 
-    // Menampilkan isi geodata di about.html jika elemennya ada
-    const geodata = document.getElementById("geodata");
-    if (geodata) {
-        geodata.innerHTML = `
+  // Menampilkan isi geodata di about.html jika elemennya ada
+  const geodata = document.getElementById("geodata");
+  if (geodata) {
+    geodata.innerHTML = `
             <p>Saya terus mengasah kemampuan dalam pengembangan web sederhana dan bercita-cita melanjutkan pendidikan tinggi ke <strong>Universitas Jember (UNEJ)</strong>.</p>
         `;
-    }
+  }
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    const contactForm = document.getElementById("contactForm");
+  const contactForm = document.getElementById("contactForm");
 
-    if (contactForm) {
-        contactForm.addEventListener("submit", function (e) {
-            e.preventDefault(); // Mencegah reload halaman
+  if (contactForm) {
+    contactForm.addEventListener("submit", function (e) {
+      e.preventDefault(); // Mencegah reload halaman
 
-            const nama = document.getElementById("nama").value;
+      const nama = document.getElementById("nama").value;
 
-            // Menampilkan Alert
-            alert(`Pesan berhasil dikirim! Terima kasih, ${nama}. Saya akan segera menghubungi Anda.`);
+      // Menampilkan Alert
+      alert(
+        `Pesan berhasil dikirim! Terima kasih, ${nama}. Saya akan segera menghubungi Anda.`,
+      );
 
-            // Mengosongkan form kembali setelah dikirim
-            contactForm.reset();
-        });
-    }
+      // Mengosongkan form kembali setelah dikirim
+      contactForm.reset();
+    });
+  }
 });
+
+// Mengaktifkan fitur CSS :active di iOS/iPhone
+document.addEventListener("touchstart", function() {}, true);
